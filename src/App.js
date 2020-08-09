9bbff4beb6b0239bf882fb2c8d1836e41efb32db
uArthurSPereira <arthursp7007@gmail.com>
2020-08-09 13:32:25 -0300

    Teste Zueira

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import Ninja from './Components/Ninja';
 export default function App() {
   return (
     <div className="App">
-      <BoasVindas />
-      <Ninja />
-      <h3>Esse é o meu primeiro deploy</h3>
+      <h2>Olá, me chamo Athur.</h2>
+      <h2>E esse menino lindo ai em baixo sou eu.</h2>
+      <h2>Um dia serei um grande programador</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/rxGnonKB7TY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h2>Agora bora escutar uma eletronica.</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/NUVCQXMUVnI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
