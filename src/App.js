3237bb4e5c0933a43da345ae8b48b1a7db798a9a
uArthurSPereira <arthursp7007@gmail.com>
2020-08-09 14:28:31 -0300

    Alteração

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <h2>Olá, me chamo Athur.</h2>
-      <h2>E esse menino lindo ai em baixo sou eu.</h2>
       <h2>Um dia serei um grande programador</h2>
       <iframe width="360" height="115" src="https://www.youtube.com/embed/rxGnonKB7TY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <h2>Agora bora escutar uma eletronica.</h2>
