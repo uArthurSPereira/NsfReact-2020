d294e4f937798f16cac759f42f90dd9a35656cda
uArthurSPereira <arthursp7007@gmail.com>
2020-08-09 12:52:19 -0300

    Primeiros passos

---
+++ src/Components/BoasVindas/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BoasVindas.css';
+
+export default function BoasVindas() {
+
+    return (
+        <div className="boasvindas">
+            <h1>
+                Sejam todos muito bem-vindos!
+            </h1>
+            <h2>
+                React na veiaaaa e sangue nos óiooooo!
+            </h2>
+        </div>
+    )
+}
---
+++ src/Components/Ninja/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Ninja.css';
+
+export default function Ninja() {
+
+    return (
+        <div className="ninja">
+            <h2>
+                Bora ser Ninjaaaaaa!
+            </h2>
+        </div>
+    )
+}
